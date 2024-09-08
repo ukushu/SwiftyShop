412054a78332560ffafff1819226b49dcd790dd3
UKS <skulptorrr@duck.com>
2024-09-08 12:50:18 +0300

    alert

--- Sources/SwiftyShop/alert.swift
+++ Sources/SwiftyShop/alert.swift
@@ -2,10 +2,12 @@ import Foundation
 import AppKit
 
 func alert(msg: String, text: String) {
-    let alert: NSAlert = NSAlert()
-    alert.messageText = msg
-    alert.informativeText = text
-    alert.alertStyle = NSAlert.Style.informational
-    alert.addButton(withTitle: "OK")
-    alert.runModal()
+    DispatchQueue.main.sync {
+        let alert: NSAlert = NSAlert()
+        alert.messageText = msg
+        alert.informativeText = text
+        alert.alertStyle = NSAlert.Style.informational
+        alert.addButton(withTitle: "OK")
+        alert.runModal()
+    }
 }
