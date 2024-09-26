dfa8a11e4ec679534976ff7aae11c6900b506d48
UKS <skulptorrr@duck.com>
2024-09-26 17:24:19 +0300

    ProductID.State

--- Sources/SwiftyShop/Relations/ProductID.State.swift
+++ Sources/SwiftyShop/Relations/ProductID.State.swift
@@ -14,17 +14,18 @@ public extension ProductID {
 
 public extension ProductID.State {
     var price : String {
-        switch self {
-        case .pending(_): ""
-        case .fetched(let product, _):
-            printDbg("fetched \(product.displayPrice)")
-            return product.displayPrice
-        case .restored(let product, _):
-            printDbg("restored \(product.displayPrice)")
-            return product.displayPrice
-        case .read(let info):
-            printDbg("read \(info.price)")
-            return info.price
-        }
+        return
+            switch self {
+            case .pending(_): ""
+            case .fetched(let product, _):
+                printDbg("fetched \(product.displayPrice)")
+                return product.displayPrice
+            case .restored(let product, _):
+                printDbg("restored \(product.displayPrice)")
+                return product.displayPrice
+            case .read(let info):
+                printDbg("read \(info.price)")
+                return info.price
+            }
     }
 }
